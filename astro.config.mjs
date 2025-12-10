9b1e7fddb0b29c8a18fb4c5f5b581c77c2dfb753
varnebla <victor.arnedo.93@gmail.com>
2025-12-10 23:45:26 +0100

    feat: Add sitemap integration and i18n configuration for improved SEO and localization

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,19 @@
 import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
+import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+  site: 'https://image-optimizer.xyz',
+  integrations: [vue(), sitemap()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+  },
 
   vite: {
     plugins: [tailwindcss()]
